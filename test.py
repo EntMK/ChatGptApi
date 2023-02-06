7de169990ef0f197d98c820ffa93fb976f3b4d9f
엔터마케팅\xzcqq <entermarketing9574@gmail.com>
2023-02-07 04:10:24 +0900

    1

---
+++ test.py
@@ -0,0 +1,20 @@
+import openai
+import os
+
+
+openai.api_key = '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'
+
+user_text = '철학이란'
+completions = openai.Completion.create(
+        engine='text-davinci-003',  # Determines the quality, speed, and cost.
+        temperature=0.5,            # Level of creativity in the response
+        prompt=user_text,           # What the user typed in
+        max_tokens=1000,             # Maximum tokens in the prompt AND response
+        n=1,                        # The number of completions to generate
+
+)
+
+text = completions.choices[0].text
+
+print(text)
+
