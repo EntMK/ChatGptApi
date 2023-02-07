1aee5e446ea6f0136dd48584ac357766d4084a46
엔터마케팅\xzcqq <entermarketing9574@gmail.com>
2023-02-07 13:34:01 +0900

    1

--- main.py
+++ main.py
@@ -29,13 +29,13 @@ async def test():
 async def gpt_message(q: Optional[str] = ''):
 
     if q == "":
-        return {"message": ''}
+        yield {"message": ''}
 
     number = answer_number.pop()
     answer_number.append(number + 1)
     await request_processing(q, str(number))
 
-    return {'question': q, 'answer_number': str(number)}
+    yield {'question': q, 'answer_number': str(number)}
 
 
 async def request_processing(q, number):
